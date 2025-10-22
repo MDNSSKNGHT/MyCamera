bf6c6db2706e6f1beea4cf99f6b75c37e011e892
Leonardo Estacio <mdnssknght@gmail.com>
2025-10-21 20:14:20 -0500

    Consolidate shader module definitions
    Instead of having a separate file for each shader module, consolidate them into `shader.rs` for better organization.

--- raw_processor/src/shader.rs
+++ raw_processor/src/shader.rs
@@ -1,2 +1,13 @@
-pub mod finishing_1;
-pub mod finishing_2;
+pub mod finishing_1 {
+    vulkano_shaders::shader! {
+        ty: "compute",
+        path: "shader/finishing_1.glsl"
+    }
+}
+
+pub mod finishing_2 {
+    vulkano_shaders::shader! {
+        ty: "compute",
+        path: "shader/finishing_1.glsl"
+    }
+}
--- raw_processor/src/shader/finishing_2.rs
+++
@@ -1,8 +0,0 @@
-mod finishing_2 {
-    vulkano_shaders::shader! {
-        ty: "compute",
-        path: "shader/finishing_2.glsl"
-    }
-}
-
-pub use finishing_2::*;
